c4431ce96145f5d85994cffe33e0b43a7f8a505c
8554955009Rutuja <rutujamohite17march@gmail.com>
2022-11-21 15:03:20 +0530

    update screenshot

--- src/ScreenshotPackage/Prog2.java
+++ src/ScreenshotPackage/Prog2.java
@@ -5,6 +5,8 @@ public class Prog2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           Vaibhav is expert in teaching automation 
+
+		  Vaibhav is brave but he doesn't accept
 	}
 
 }
